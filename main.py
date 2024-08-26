4a6149664038f126dced509afd82f1a3177000b3
NeekoKun <leonardo.riccim@gmail.com>
2024-08-26 18:43:19 +0200

    Removed map warping, added interactive map color and heat map generation

--- main.py
+++ main.py
@@ -11,14 +11,39 @@ class Game:
 
         self.camera_speed = self.settings["camera speed"]
 
+        self.DESERT = 9
+        self.PLAIN = 10
+        self.TUNDRA = 11
+        self.SNOW = 12
+        self.DESERT_HILL = self.DESERT + 100
+        self.PLAIN_HILL = self.PLAIN + 100
+        self.TUNDRA_HILL = self.TUNDRA + 100
+        self.SNOW_HILL = self.SNOW + 100
+        self.MOUNTINE = 13
+        self.SEA = 14
+
         logging.basicConfig(format="%(levelname)s:%(name)s:%(message)s", filename="game.log")
 
+    def init_colors(self):
+        curses.start_color()
+
+        curses.init_color(self.DESERT, 969, 863, 725)
+        curses.init_color(self.DESERT_HILL, 871, 675, 502)
+        curses.init_color(self.PLAIN, 506, 635, 388)
+        curses.init_color(self.PLAIN_HILL, 212, 369, 196)
+        curses.init_color(self.TUNDRA, 251, 325, 298)
+        curses.init_color(self.TUNDRA_HILL, 412, 459, 396)
+        curses.init_color(self.SNOW, 957, 957, 949)
+        curses.init_color(self.SNOW_HILL, 910, 910, 910)
+        curses.init_color(self.MOUNTINE, 208, 216, 294)
+        curses.init_color(self.SEA, 12, 204, 431)
+        
     def start(self):
         curses.wrapper(self.GameLoop)
 
-    def generateMap(self, width, height, shape='island', temperature=0.5, wtl=0.5):
+    def generateMap(self, width, height, shape='is', temperature=0):
         self.map = Map(width, height)
-        self.map.generateNewMap((width, height), shape='shape')
+        self.map.generateNewMap(shape=shape, temperature=temperature)
 
     def save(self):
         raw = self.map.getRaw()
@@ -29,45 +54,74 @@ class Game:
     def load(self):
         with open(self.settings["saveFile"], 'r') as save:
             grid = list(map(str.strip, save.readlines()))
-            
         
         self.map = Map()
         self.map.loadFromRaw(grid)
 
     def draw_map(self):
-        for y, row in enumerate(self.map.grid):
+        for y, row in enumerate(self.map.elevation_grid):
             for x, cell in enumerate(row):
-                self.draw_hexagon(4*y + 2*(x%2), 2+8*x, str(cell))
+                properties = {}
 
-    def draw_hexagon(self, y, x, char: str):
-        self.map_pad.addstr(y,   x,     "·-----·")
-        self.map_pad.addstr(y+1, x-1,  "/       \\")
-        self.map_pad.addstr(y+2, x-2, "·    "+char+"    ·")
-        self.map_pad.addstr(y+3, x-1,  "\\       /")
-        self.map_pad.addstr(y+4, x,     "·-----·")
+                properties["elevation"] = cell
+                properties["heat"] = self.map.heat_grid[y][x]
 
+                self.draw_hexagon(4*y + 2*(x%2), 2+8*x, properties)
+
+    def draw_hexagon(self, y, x, properties: dict):
+        # Draw frame
+        # TODO: modify frame color in function of country borders
+
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+        self.map_pad.addstr(y,   x,     "·-----·",   curses.color_pair(1))
+        self.map_pad.addstr(y+1, x-1,  "/       \\", curses.color_pair(1))
+        self.map_pad.addstr(y+2, x-2, "·         ·", curses.color_pair(1))
+        self.map_pad.addstr(y+3, x-1,  "\\       /", curses.color_pair(1))
+        self.map_pad.addstr(y+4, x,     "·-----·",   curses.color_pair(1))
+
+        # Draw content
+
+        match properties["elevation"]:
+            case 0:
+                color = self.SEA
+            case 1:
+                color = properties["heat"] + 9
+            case 2:
+                color = properties["heat"] + 109
+            case 3:
+                color = self.MOUNTINE
+
+        logging.warning(color)
+
+        curses.init_pair(color, curses.COLOR_WHITE, color)
+
+        logging.warning(curses.color_pair(2))
+
+        self.map_pad.addstr(y+1, x,    "       ",  curses.color_pair(color))
+        self.map_pad.addstr(y+2, x-1, "    "+str(properties["heat"])+"    ", curses.color_pair(color))
+        self.map_pad.addstr(y+3, x,    "       ",  curses.color_pair(color))
+                
     def print_map(self, stdscr):
-
-        if self.max_position[0] - self.current_position[0] < self.cols:
-            self.map_pad.overwrite(stdscr, self.current_position[1], self.current_position[0], 0, 0, self.rows - 1, self.max_position[0] - self.current_position[0])
-            self.map_pad.overwrite(stdscr, self.current_position[1], 0, 0, self.max_position[0] - 3 - self.current_position[0], self.rows - 1, self.cols - 1)
-        else:
-            self.map_pad.overwrite(stdscr, self.current_position[1], self.current_position[0], 0, 0, self.rows - 1, self.cols - 1)
+        self.map_pad.refresh(self.current_position[1], self.current_position[0], 0, 0, self.rows - 1, self.cols - 1)
 
     def GameLoop(self, stdscr):
         curses.curs_set(False)
+
+        self.init_colors()
+
         self.map_pad = curses.newpad(3 + 4*self.map.height, 3 + 8*self.map.width)
 
         self.draw_map()
         self.rows, self.cols = stdscr.getmaxyx()
         self.current_position = [0, 0]
 
-        self.max_position = (3 + 8*self.map.width, 3 + 4*self.map.height)
+        self.max_position = ((3 + 8*self.map.width) - (self.cols), (3 + 4*self.map.height) - (self.rows))
 
         while True:
+            stdscr.refresh()
             self.print_map(stdscr)
 
-            stdscr.refresh()
 
             cmd = stdscr.getch()
 
@@ -75,24 +129,27 @@ class Game:
                 case curses.KEY_UP:
                     if self.current_position[1] - self.camera_speed >= 0:
                         self.current_position[1] -= self.camera_speed
+                    else:
+                        self.current_position[1] = 0
                 case curses.KEY_DOWN:
                     if self.current_position[1] + self.camera_speed <= self.max_position[1]:
                         self.current_position[1] += self.camera_speed
+                    else:
+                        self.current_position[1] = self.max_position[1]
                 case curses.KEY_LEFT:
                     if self.current_position[0] - 2*self.camera_speed >= 0:
                         self.current_position[0] -= 2*self.camera_speed
                     else:
-                        self.current_position[0] = self.max_position[0] - (2*self.camera_speed - self.current_position[0])
+                        self.current_position[0] = 0
                 case curses.KEY_RIGHT:
                     if self.current_position[0] + 2*self.camera_speed < self.max_position[0]:
                         self.current_position[0] += 2*self.camera_speed
                     else:
-                        self.current_position[0] = self.current_position[0] + 2*self.camera_speed - self.max_position[0]
+                        self.current_position[0] = self.max_position[0]
                 case _:
                     pass
-            
-            
-            
+
+
 new_game = Game()
-new_game.generateMap(100, 60)
+new_game.generateMap(100, 100, temperature=0.2)
 new_game.start()
--- map.py
+++ map.py
@@ -1,15 +1,19 @@
+import pygame
 import logging
 import random
+import noise.perlin
 import numpy as np
 import noise
 import math
 
 class Map:
-    def __init__(self, width=0, height=0) -> None:
+    def __init__(self, width, height) -> None:
         self.width = width
         self.height = height
-        logging.basicConfig(filename='map.log')
-
+        self.HOT = 0
+        self.WARM = 1
+        self.COLD = 2
+        self.ICE = 3
 
     def generate_island_mask(self, center, radius):
         self.mask = []
@@ -22,42 +26,56 @@ class Map:
                     self.mask[y][-1] = 1
                 self.mask[y][-1] = round(1 - self.mask[y][-1], 2)
 
-    """
-    def generate_archipelago_mask(self, x, y, seed=0):
-        random.seed = seed
+    def display_matrix(self, matrix: list):
+        pygame.display.init()
+        scale = 1000 // len(matrix), 1000 // len(matrix[0])
+        screen = pygame.display.set_mode((scale[0] * len(matrix), scale[1] * len(matrix[0])))
 
-        if not self.islands:
-            self.islands = [random.randint(), random.randint() for _ in range(random.randint(5, 20))]
-"""
-    def generateNewMap(self, size: list, scale=30, seed=3, radius=10, shape='island'):
-        self.grid = []
+        for y, row in enumerate(matrix):
+            for x, cell in enumerate(row):
+                print(cell)
+                pygame.draw.rect(screen, (255*cell/3, 255*cell/3, 255*cell/3), pygame.Rect(x*scale[0], y*scale[1], scale[0], scale[1]))
+        
+        pygame.display.flip()
+
+        while True:
+            pass
+
+    def generateNewMap(self, size = None, scale=30, seed=3, radius=10, shape='island', temperature=0):
+        if size == None:
+            size = (self.width, self.height)
+
+        self.height = size[1]
+        self.width = size[0]
+        self.elevation_grid = []
 
         for i in range(size[1]):
-            self.grid.append([])
+            self.elevation_grid.append([])
 
-        # generate basic elevation map
+        ### Generate basic elevation grid ###
         elevation_grid = []
+
         for i in range(size[1]):
             elevation_grid.append([])
             for j in range(size[0]):
                 elevation_grid[i].append(noise.pnoise3(
-                    (4*size[0] / math.pi) * math.sin((8*j) * (2 * math.pi) / (8*size[0])) / scale,
-                    (4*size[0] / math.pi) * math.cos((8*j) * (2 * math.pi) / (8*size[0])) / scale,
+                    (4*size[0] / math.pi) * math.sin(2*j * math.pi / size[0]) / scale,
+                    (4*size[0] / math.pi) * math.cos(2*j * math.pi / size[0]) / scale,
                     (4*i + 2*(j%2)) / scale,
-                    base=seed) + 0.5)
+                    base=seed, octaves=3, lacunarity=2, persistence=0.5
+                    ))
 
-        self.generate_island_mask((len(elevation_grid[0])/2, len(elevation_grid)/2), 70)
-        for row in self.mask:
-            logging.warning(row)
+        mn = min([min(row) for row in elevation_grid])
+        elevation_grid = [[i - mn for i in row] for row in elevation_grid]
+        mx = max([max(row) for row in elevation_grid])
+        elevation_grid = [[i / mx for i in row] for row in elevation_grid]
+
+
+        # Apply shape mask
 
         match shape:
             case 'island':
                 self.generate_island_mask((len(elevation_grid[0])/2, len(elevation_grid)/2), 70)
-            case 'archipelago':
-                for y, row in enumerate(elevation_grid):
-                    for x, cell in enumerate(row):
-                        elevation_grid[y][x] = cell - self.archipelago_function(x, y+0.5%2, seed)
-
             case _:
                 self.mask = [[1 for _ in range(self.width)] for _ in range(self.height)]
 
@@ -67,36 +85,93 @@ class Map:
             for cell in row:
                 match cell:
                     case cell if cell < 0.5:
-                        self.grid[y].append(' ')
+                        self.elevation_grid[y].append(0)
                     case cell if 0.5 <= cell < 0.7:
-                        self.grid[y].append(1)
+                        self.elevation_grid[y].append(1)
                     case cell if 0.7 <= cell < 0.9:
-                        self.grid[y].append(3)
+                        self.elevation_grid[y].append(2)
                     case cell if 0.9 <= cell:
-                        self.grid[y].append('#')
-        
-        self.height = len(self.grid)
-        self.width = len(self.grid[0])
+                        self.elevation_grid[y].append(3)
+
+        ### Generate heat grid ###
+
+        # Plain latitudinal map
+        heat_map = [[((abs(i + 0.5*(j%2) - self.height/2)) / (self.height/2)) - temperature for j in range(self.width)] for i in range(self.height)]
+
+        # Perturbate with 3d perlin noise
+        turbolence_dim = 3
+
+        turbolence_map = [[
+                        noise.pnoise3(
+                        (4*self.width / math.pi) * math.sin((2*x) * math.pi / self.width) / scale,
+                        (4*self.width / math.pi) * math.cos((2*x) * math.pi / self.width) / scale,
+                        (4*y + 2*(x%2)) / scale,
+                        base=seed+1) for x, cell in enumerate(row)
+                    ] for y, row in enumerate(heat_map)]
+
+        # Normalize and dim turbolence map 
+
+        mn = min([min(row) for row in turbolence_map])
+        turbolence_map = [[i - (mn - turbolence_dim) for i in row] for row in turbolence_map]
+        mx = max([max(row) for row in turbolence_map])
+        turbolence_map = [[i / mx for i in row] for row in turbolence_map]
+
+        # Apply turbolence
+
+        heat_map = [[cell * turbolence_map[y][x] for x, cell in enumerate(row)] for y, row in enumerate(heat_map)]
+
+        # Generate grid from map
+
+        self.heat_grid = []
+
+        hot_threshold = 0.2
+        warm_threshold = 0.6
+        cold_threshold = 0.8
+
+        for row in heat_map:
+            self.heat_grid.append([])
+            for cell in row:
+                match cell:
+                    case cell if cell < hot_threshold:
+                        self.heat_grid[-1].append(self.HOT)
+                    case cell if hot_threshold <= cell < warm_threshold:
+                        self.heat_grid[-1].append(self.WARM)
+                    case cell if warm_threshold <= cell < cold_threshold:
+                        self.heat_grid[-1].append(self.COLD)
+                    case cell if cold_threshold <= cell:
+                        self.heat_grid[-1].append(self.ICE)
+
+        self.display_matrix(self.heat_grid)
+
+        ### Generate biome grid ###
+
+
+
+        ### Generate Fog of War map ###
+
+        self.fow_grid = [[1 for _ in range(self.width)] for _ in range(self.height)]
+
+
 
     def loadFromRaw(self, raw: list) -> None:
         # Check formatting
         if not len(set(map(len, raw))) <= 1:
             raise TypeError("Raw list not correctly formatted")
         
-        self.grid = []
+        self.elevation_grid = []
 
         for i in range(len(raw[0])):
-            self.grid.append([])
+            self.elevation_grid.append([])
 
         for column in raw:
             for i, cell in enumerate(column):
-                self.grid[i].append(cell)
+                self.elevation_grid[i].append(cell)
         
         # Check for grid values
         pass
 
-        self.height = len(self.grid)
-        self.width = len(self.grid[0])
+        self.height = len(self.elevation_grid)
+        self.width = len(self.elevation_grid[0])
     
     def getRaw(self) -> list:
         raw = []
@@ -104,8 +179,12 @@ class Map:
         for i in range(self.width):
             raw.append("")
 
-        for column in self.grid:
+        for column in self.elevation_grid:
             for i, cell in enumerate(column):
                 raw[i] += str(cell)
         
         return raw
+
+if __name__ == "__main__":
+    map = Map(100, 100)
+    map.generateNewMap()
